8ecf07843cef78c9a7b0200cce559ceac5d81bef
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-08-06 22:44:17 +0200

    1/2 tasks

---
+++ 011.Num/11/a.js
@@ -0,0 +1,9 @@
+/* Напишите программу, в которой вычисляется сумма, разность и произведение
+двух целых чисел, введенных с клавиатуры.
+ */
+
+let a = +prompt();
+let b = +prompt();
+console.log('Summa: ', a + b);
+console.log('Raznost: ', a - b);
+console.log("Proizvedenie:" , a * b);
