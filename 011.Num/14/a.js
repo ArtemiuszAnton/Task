6ac74ac2fbd99cce0a8e67c822f0cf7f978faa4c
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-08-07 14:11:55 +0200

    18/19 tasks

---
+++ 011.Num/14/a.js
@@ -0,0 +1,10 @@
+/* Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в
+противном случае false
+ */
+
+let a = prompt();
+if (a % 2 === 0) {
+    console.log(Boolean(true))
+} else {
+    console.log(Boolean(false));
+}
