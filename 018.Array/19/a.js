0c06feeea992afb80411e64b00fdfce40cb3cf71
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-08-29 22:05:02 +0200

    018 Done, without 16 task

--- 018.Array/19/a.js
+++ 018.Array/19/a.js
@@ -1,14 +1,46 @@
-/* Пользователь вводит число, явдяющееся количеством элементов будущего
-массива. Напишите код заполнения массива методом prompt. Проверить каждый
-prompt на число (ввод строки, проверка на isNaN)
+/* На входе n – количество элементов массива. Далее производится заполнение
+массива с клавиатуры (хотя бы один элемент массива – 0). Необходимо вывести
+сумму всех значений массива до первого 0. Добавить проверки на ввод чисел.
 */
 
-let num = +prompt('Введите количество элементов массива')
-let res = []
-
-for (let i = 0; i < num; i++) {
-    let pr = prompt('Введите эл массива')
-    if (isNaN(pr)) {
-res.push(pr)
+let n = 4;
+let arr = [];
+for (let i = 0; i < n; i++) {
+    let num = prompt('Введите эл массива, один из которых 0')
+    if (!isNaN(num)) {
+        arr.push(+num)
     }
-} console.log(res);
+}
+
+
+let res = 0
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+        break
+    } else {
+        res += arr[i]
+    }
+
+}
+console.log(res);
+
+
+
+// let res = 0
+// arr.forEach(function (elem) {
+//     if (elem !== 0) {
+//         res += elem
+//     } else {
+
+//     }
+// })
+
+let isNul = false;
+let res_2 = arr.reduce(function (sum, elem) {
+    if (elem === 0 || isNul == true) {
+        isNul = true
+        return sum + 0;
+    } else {
+        return sum + elem
+    }
+}, 0)
