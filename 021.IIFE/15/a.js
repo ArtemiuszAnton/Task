701febca33b3bea09ca9cafb16c3ddee48134bd3
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-09-16 23:13:48 +0200

    edit task 021

--- 021.IIFE/15/a.js
+++ 021.IIFE/15/a.js
@@ -1,21 +1,19 @@
-/* Напишите функцию, генерирующую пароль из 8 символов с использованием
-замыкния. (Math.random)
+/* 
  */
 
-function getPass() {
-    let str = '';
-    return () => {
-        str += Math.floor(Math.random() * 9);
-        console.log(str);
-    }
+
+const arr = [1, 2, 3, 4, 5, 'raz', 'dwa'];
+let num = [];
+let str = [];
+
+for (let i = 0; i < arr.length; i++) {
+   if (!isNaN(arr[i])) {
+    num.push(arr[i])
+   } else {
+    str.push(arr[i])
+   }
+    
 }
 
-let res = getPass()
-
-
-res()
-res()
-res()
-res()
-res()
-res()
+console.log(str);
+console.log(num);
