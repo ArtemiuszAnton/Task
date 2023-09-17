141477e8b16cbdccbc224945eee85b46c9ebff19
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-09-17 10:48:22 +0200

    refact 021

--- 021.IIFE/15/a.js
+++ 021.IIFE/15/a.js
@@ -2,18 +2,3 @@
  */
 
 
-const arr = [1, 2, 3, 4, 5, 'raz', 'dwa'];
-let num = [];
-let str = [];
-
-for (let i = 0; i < arr.length; i++) {
-   if (!isNaN(arr[i])) {
-    num.push(arr[i])
-   } else {
-    str.push(arr[i])
-   }
-    
-}
-
-console.log(str);
-console.log(num);
