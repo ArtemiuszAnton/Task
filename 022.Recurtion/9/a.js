a713600ab6534f3a14bbc3a25a88954c342fe682
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-09-20 22:51:49 +0200

    edit tasks 022

--- 022.Recurtion/9/a.js
+++ 022.Recurtion/9/a.js
@@ -5,30 +5,3 @@
 возвращающую массив с удвоенными элементами
  */
 
-let arr = [2, 3, 4, 5, 66, 77, 8, 12];
-
-
-function checkArr(arr) {
-    let checker = arr.every(function (elem) {
-        if (!isNaN(elem)) {
-            return true
-        }
-    })
-    return checker
-}
-
-
-function getMultiply(arr) {
-    const check = checkArr(arr);
-    const finArr = [];
-    if (check === true) {
-        for (let i = 0; i < arr.length; i++) {
-            finArr.push(arr[i] * 2)
-        }
-    } else {
-        return 'error'
-    }
-    return finArr
-}
-let fin = getMultiply(arr)
-console.log(fin);
