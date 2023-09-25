7aa10fe7516645596f2a3d3a53dcb1f9178e47e7
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-09-25 23:39:56 +0200

    edit tasks

--- 023.Algorithm/5/a.js
+++ 023.Algorithm/5/a.js
@@ -1,18 +1,4 @@
 /* 
-5. Напишите функцию countNumbers(arr), которая считает количество чисел в
-массиве arr. Используйте рекурсию.
+
  */
 
-let arr = [23, 3, 4, 54, 6576, 87, 98, 93];
-let count = 0;
-
-function countNumbers(arr) {
-        
-        if (!arr.length) return;
-count++
-        countNumbers(arr.slice(1))
-
-}
-
-countNumbers(arr);
-console.log(count);
