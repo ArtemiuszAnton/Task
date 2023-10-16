a8a61cbe76a263fb0b0cc9afba2c4ae4c81c80d2
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-10-16 10:15:24 +0200

    edit 5 task 032

--- 032.OOP/5/a.js
+++ 032.OOP/5/a.js
@@ -4,11 +4,82 @@
 
  */
 
+// class Anagram {
+//     first;
+//     sec;
+
+//     validAnagram = (first, sec) => {
+//         try {
+//             if(!isNaN(first || sec)) throw new Error('not valid')
+//             if (first.split('').sort().join('') == sec.split('').sort().join('')) return true
+//             else return false 
+//         } catch (error) {
+//             return error.message
+//         }
+
+//     }
+// }
+
+// let anagram = new Anagram;
+// console.log(anagram.validAnagram('rake', 'aker'));
+
+// -------------------------------------------------------------------------
+
+// class Anagram {
+//     first = 'akr';
+//     sec = 'rak';
+
+//     
+
+//     }
+// }
+
+// let anagram = new Anagram;
+// console.log(anagram.validAnagram());
+
+// --------------------------------------------------------------------------
+
+// class Anagram {
+//     MyFirst;
+//     MySec;
+
+//     constructor(first, sec) {
+//         this.MyFirst = first
+//         this.MySec = sec
+//     }
+
+//     validAnagram = () => {
+//         try {
+//             if (!isNaN(this.MyFirst || this.MySec)) throw new Error('not valid')
+//             if (this.MyFirst.split('').sort().join('') == this.MySec.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             return error.message
+//         }
+//     }
+// }
+// let anagram = new Anagram('rake', 'aker');
+// console.log(anagram.validAnagram());
+
+// -------------------------------------------------------------------------
+
 class Anagram {
     first;
     sec;
 
-    validAnagram = ()=>{
+    validAnagram = () => {
+        try {
+            if(!isNaN(this.first || this.sec)) throw new Error('not valid')
+            if (this.first.split('').sort().join('') == this.sec.split('').sort().join('')) return true
+            else return false 
+        } catch (error) {
+            return error.message
+        }
 
     }
 }
+
+let anagram = new Anagram;
+anagram.first = 'akr';
+anagram.sec = 'rak'
+console.log(anagram.validAnagram());
