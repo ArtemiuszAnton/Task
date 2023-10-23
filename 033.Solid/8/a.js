29d27d775602bbb428b980a7421e856ebb8179ac
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-10-23 23:40:54 +0200

    task 8 edit 033

--- 033.Solid/8/a.js
+++ 033.Solid/8/a.js
@@ -9,36 +9,9 @@ const p = document.querySelector('p')
 
 
 class DomHtml {
-   // data = [
-   //    { "id": 1, "name": "Yesenia", "age": 22 },
-   //    { "id": 2, "name": "Hanna", "age": 22 },
-   //    { "id": 3, "name": "Stanislau", "age": 25 },
-   //    { "id": 4, "name": "German", "age": 18 },
-   //    { "id": 5, "name": "Maria", "age": 27 }
-   // ]
-  
-   }
-   repository(json) {
-      const data = [
-         { "id": 1, "name": "Yesenia", "age": 22 },
-         { "id": 2, "name": "Hanna", "age": 22 },
-         { "id": 3, "name": "Stanislau", "age": 25 },
-         { "id": 4, "name": "German", "age": 18 },
-         { "id": 5, "name": "Maria", "age": 27 }
-      ]
-
-      btn.addEventListener('click', () => {
-         const check = data.some(el => {
-            if (json.id == el.id) p.textContent = 'true'
-         })
-        
-      })
-      return check
-   }
 }
 
 const domhtml = new DomHtml();
-const json = JSON.parse(inp.value)
-console.log(domhtml.middleware(json));
+
 
 
