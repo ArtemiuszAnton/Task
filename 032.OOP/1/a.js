a8e176acfe22fd1f5896051e8de4a34825b6371a
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-10-30 23:18:06 +0100

    edit tasks

--- 032.OOP/1/a.js
+++ 032.OOP/1/a.js
@@ -3,43 +3,43 @@ surname. Также класс должен иметь метод getAutograph, 
 “{name} {surname), с наилучшими пожеланиями”. 
  */
 
-// class Singer {
-//     name = 'Artem';
-//     surname = 'Anton';
-//     getAutograph = () => {
-//         return `${this.name} ${this.surname}`
-//     }
-// }
-
-// const singer = new Singer();
-// console.log(singer.getAutograph());
-
-// class Singer {
-//     name;
-//     surname;
-//     getAutograph=()=>{
-//         return `${this.name} ${this.surname}`
-//     }
-// }
-
-// const singer = new Singer();
-// singer.name = 'Artem';
-// singer.surname = 'Anton';
-
-// console.log(singer.getAutograph());
-
 class Singer {
-    myName;
-    mySurname;
-
-    constructor(name, surname) {
-        this.myName = name
-        this.mySurname = surname
-    }
+    name = 'Artem';
+    surname = 'Anton';
     getAutograph = () => {
-        return `${this.myName} ${this.mySurname}`
+        return `${this.name} ${this.surname}`
     }
 }
 
-const singer = new Singer('artem', 'anton');
+const singer = new Singer();
 console.log(singer.getAutograph());
+
+class Singer {
+    name;
+    surname;
+    getAutograph=()=>{
+        return `${this.name} ${this.surname}`
+    }
+}
+
+const singer = new Singer();
+singer.name = 'Artem';
+singer.surname = 'Anton';
+
+console.log(singer.getAutograph());
+
+// class Singer {
+//     myName;
+//     mySurname;
+
+//     constructor(name, surname) {
+//         this.myName = name
+//         this.mySurname = surname
+//     }
+//     getAutograph = () => {
+//         return `${this.myName} ${this.mySurname}`
+//     }
+// }
+
+// const singer = new Singer('artem', 'anton');
+// console.log(singer.getAutograph());
