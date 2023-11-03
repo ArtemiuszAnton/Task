9452ebc27e9cbe8998ff754f7166703d03edd545
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-11-03 23:07:35 +0100

    edit tasks

--- 036.Repeat/8/a.js
+++ 036.Repeat/8/a.js
@@ -2,10 +2,3 @@
  каждый item списка. Необходимо отловить на какой из элементов нажал
 пользователь и отобразить в div расположенном ниже маркированного списка */
 
-const list = document.querySelector('ul');
-const p = document.querySelector('p');
-
-
-list.addEventListener('click', (event) => {
-    p.innerHTML = event.target.textContent
-})
