2f2aaadf6972616ca5faddc7e6ecc41731d52b5e
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-11-12 09:49:04 +0100

    replace 9 task

--- 036.Repeat/9/a.js
+++ 036.Repeat/9/a.js
@@ -6,26 +6,7 @@
 
 
 
-document.querySelector('button').addEventListener('click', () => {
-    const p = document.querySelector('p');
-    const div = document.querySelector('div')
-    const arr = [];
-    const nArr = [];
-    for (let i = 0; i < 15; i++) {
-        arr.push(Math.round(Math.random() * 100))
-    }
 
-    for (let i = 0; i < arr.length; i++) {
-        const li = document.createElement('li')
-        li.innerHTML = arr[i]
-        p.appendChild(li)
-    }
-    p.addEventListener('click', (event) => {
-        p.removeChild(event.target)
-        nArr.push(event.target.textContent)
-        div.innerHTML = nArr
-    })
-})
 
 
 
