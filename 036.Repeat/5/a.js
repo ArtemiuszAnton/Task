6dfeef54d81ad44db0c0c14ab7e3305a9f157386
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-11-16 20:41:55 +0100

    replace tks

--- 036.Repeat/5/a.js
+++ 036.Repeat/5/a.js
@@ -13,7 +13,7 @@ btn.addEventListener('click', () => {
     try {
         if (isNaN(inp.value)) throw new Error('not a number')
         if (inp.value < 0) throw new Error('меньше нуля')
-        else return p.textContent = inp.value
+        else return p.textContent = inp.value *2
     } catch (error) {
         return p.textContent = error.message
     }
