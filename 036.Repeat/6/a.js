f56321ed9d3406185a14a12da82c213c67e0ca63
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-11-19 23:50:06 +0100

    replay 6 task

--- 036.Repeat/6/a.js
+++ 036.Repeat/6/a.js
@@ -2,7 +2,19 @@
 проверку, что поле не пустое
  */
 
+const btn = document.querySelector('button');
+const inp = document.querySelector('input');
+const p = document.querySelector('p');
 
+btn.addEventListener('click',()=>{
+    try {
+        if(!inp.value) throw new Error('input is empty');
+        p.textContent += inp.value + ' '
+        inp.value = ''
+    } catch (error) {
+        alert(error.message)
+    }
+})
 
 
 
