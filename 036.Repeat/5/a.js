8c7396bfa43e1eb690e1ac849e2f92709407bb1b
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-11-20 23:48:33 +0100

    edit task

--- 036.Repeat/5/a.js
+++ 036.Repeat/5/a.js
@@ -9,14 +9,4 @@ const inp = document.querySelector('input');
 const p = document.querySelector('p')
 
 
-btn.addEventListener('click', () => {
-    try {
-        if (isNaN(inp.value)) throw new Error('not a number')
-        if (inp.value < 0) throw new Error('меньше нуля')
-        else return p.textContent = inp.value *2
-    } catch (error) {
-        return p.textContent = error.message
-    }
-})
-
 
