a4bb98c0dd9406e08ea05171ee281882291dc0ca
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-12-10 23:53:34 +0100

    new task 052

---
+++ 052.Node.js RestFull/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { getAllData, getId, updateById, deleteById } = require('./service');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    try {
+        const data = getAllData();
+        res.status(200).send(data)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+});
+
+app.get('/:id', (req, res) => {
+    try {
+        const { id } = req.params;
+        const get = getId(id);
+        res.status(200).send(get)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+app.put('/:id', (req, res) => {
+    try {
+        const { id } = req.params;
+        const { label, category, priority } = req.body;
+        const update = updateById(id, label, category, priority);
+        res.status(200).send(update)
+    } catch (error) {
+        res.status(404).send(error.message)
+
+    }
+});
+
+app.delete('/:id', (req, res) => {
+    try {
+        const { id } = req.params;
+        const del = deleteById(id);
+        res.status(200).send(del)
+    } catch (error) {
+        res.status(404).send(error.message)
+
+    }
+})
+
+app.listen(3000, () => {
+    console.log('server is run');
+})
+
---
+++ 052.Node.js RestFull/service.js
@@ -0,0 +1,60 @@
+const json = [
+    { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+    { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+    { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+    { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+    { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 },
+    { "id": "firebasestorage", "label": "Firebase Storage", "category": "databases", "priority": 2 },
+    { "id": "ibmcloudant", "label": "IBM Cloudant", "category": "databases", "priority": 2 },
+    { "id": "react", "label": "React", "category": "webTechnologies", "priority": 1 },
+    { "id": "angularjs", "label": "ANGULARJS", "category": "webTechnologies", "priority": 1 },
+    { "id": "redux", "label": "Redux", "category": "webTechnologies", "priority": 1 },
+    { "id": "materialui", "label": "Material UI", "category": "webTechnologies", "priority": 1 },
+    { "id": "git", "label": "Git", "category": "otherSkills", "priority": 2 },
+    { "id": "adobephotoshop", "label": "Adobe Photoshop", "category": "otherSkills", "priority": 2 },
+];
+
+function getAllData() {
+    if (!json.length) {
+        throw new Error('json is empty');
+    }
+    return json
+}
+
+function getId(id) {
+    const filt = json.filter(el => el.id == id);
+    if (!filt.length) {
+        throw new Error('this id not found')
+    }
+    return filt;
+};
+
+function updateById(id, label, category, priority) {
+    let flag = false
+    for (let i = 0; i < json.length; i++) {
+        if (json[i].id.toLowerCase() == id.toLowerCase()) {
+            flag = true
+            json[i].label = label
+            json[i].category = category
+            json[i].priority = priority
+        }
+    }
+    if (!flag) throw new Error('this id not found');
+    return json
+}
+
+function deleteById(id) {
+    const deleteElem = json.filter(el => el.id.toLowerCase() != id);
+    if(json.length == deleteElem.length) throw new Error('this id not found');
+    return deleteElem
+}
+
+
+
+
+
+
+
+
+
+module.exports = { getAllData, getId, updateById, deleteById }
